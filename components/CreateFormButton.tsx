45df35357a56ca9b2a505b8066693f78b11eb6f2
Andrzej <slobodjanik1997@gmail.com>
2024-10-18 18:00:17 +0200

    stats card, prisma client

---
+++ components/CreateFormButton.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { BsFileEarmarkPlus } from "react-icons/bs";
+import { ImSpinner2 } from "react-icons/im";
+import { Button } from "./ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+const CreateFormButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Create new form</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create form</DialogTitle>
+          <DialogDescription>
+            Create a new form to start collecting responses
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateFormButton;
